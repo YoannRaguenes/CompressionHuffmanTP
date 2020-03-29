c54ae65ff2967be0504060bea5cc539df51bc40a
YoannRaguenes <58259678+YoannRaguenes@users.noreply.github.com>
2020-03-29 16:31:16 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,10 +9,10 @@ public class Main {
 		System.out.println(alice.openFile("C:/alice.txt"));
 		alice.Alphabet();
 		alice.SortedAlphabet();
-		alice.nodeList();
+		System.out.println(alice.nodeList());
 		Node n1=alice.createTree();
 		alice.coding(n1, "");
-		
+		System.out.println(alice.getHmap());
 		//alice.codeFile("C:\\");
 		
 		
