493d6db8fc97d78c3aca10fe5136166a77e2ff0f
YoannRaguenes <58259678+YoannRaguenes@users.noreply.github.com>
2020-03-29 16:15:41 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,10 +6,15 @@ import java.awt.*;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		TxtFile alice = new TxtFile("C:/alice.txt");
-		//System.out.println(alice.openFile("C:/alice.txt"));
+		System.out.println(alice.openFile("C:/alice.txt"));
 		alice.Alphabet();
 		alice.SortedAlphabet();
-		System.out.println(alice.nodeList());
+		alice.nodeList();
+		Node n1=alice.createTree();
+		alice.coding(n1, "");
+		
+		alice.codeFile("C:\\");
+		
 		
 		
 }
